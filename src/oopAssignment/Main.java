0d3e6fdbbd4cebabab6c120d17727e6069c3c820
NataliaGarciaArias <Natalia.GarciaArias21@ycdsbk12.ca>
2021-06-02 17:52:39 -0400

    created main class

---
+++ src/oopAssignment/Main.java
@@ -0,0 +1,11 @@
+package oopAssignment;
+
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
