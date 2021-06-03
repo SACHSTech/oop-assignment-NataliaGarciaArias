a5e1efcd33f49d8578f61d4dd80ace5fc2e68c59
NataliaGarciaArias <Natalia.GarciaArias21@ycdsbk12.ca>
2021-06-03 11:15:30 -0400

    added menu method

--- src/oopAssignment/Main.java
+++ src/oopAssignment/Main.java
@@ -1,11 +1,31 @@
 package oopAssignment;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader key = new BufferedReader(new InputStreamReader(System.in));
 
-        
+        System.out.println("  ");
+    }
+
+    private void displayMenu(){
+       
+        String menuTitle = "Menu Title";
+        ArrayList<String> menuOptions = new ArrayList<String>();
+        menuOptions.add("View My List");
+        menuOptions.add("Edit My List");
+        menuOptions.add("View My Recommendations");
+
+
+        System.out.println("   "+ menuTitle +"    ");
+        System.out.println("--------------------");
+
+         for (int i = 0; i < menuOptions.size(); i++){
+             
+            System.out.println("  " + (i + 1) + " - " + menuOptions.get(i));
+         }
+
     }
 }
