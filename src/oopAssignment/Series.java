27f14e99e446411f55a37e28dfbff60b57afc333
NataliaGarciaArias <Natalia.GarciaArias21@ycdsbk12.ca>
2021-06-04 11:36:15 -0400

    series added watch time and display info methods

--- src/oopAssignment/Series.java
+++ src/oopAssignment/Series.java
@@ -43,5 +43,23 @@ public class Series extends Media {
         this.avgEpisodeDuration = newDuration;
         return avgEpisodeDuration;
     }
+
+    //Calculates estimated watch time for the series based on # of episodes and avg duration
+    public int estWatchTime(){
+       
+        int time = avgEpisodeDuration * numSeasons;
+
+        return time;
+    }
     
+    //Displays TV Series Information
+    public void displayInformation(){
+
+        System.out.println("Series Title: " + super.getTitle());
+        System.out.println("Genre: " + super.getGenre());
+        System.out.println("Year Released: " + super.getReleaseYear());
+        System.out.println("Seasons: " + numSeasons);
+        System.out.println("Estimated Watch Time: " + estWatchTime());
+
+    }
 }
